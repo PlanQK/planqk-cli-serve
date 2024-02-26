ff87bf2121532dc25108546f29a5252909414ab3
radufalcusan <radu.falcusan@anaqor.io>
2024-02-26 15:05:28 +0100

    fix: add comments to the TaskExecutor class

--- src/TaskExecutor.py
+++ src/TaskExecutor.py
@@ -1,10 +1,22 @@
 import concurrent.futures
 
+# The TaskExecutor is used as a container of future objects.
 class TaskExecutor(concurrent.futures.ThreadPoolExecutor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._futures = {} 
 
+    """
+    Submits an async task for execution.
+        Parameters:
+            fn (callable): The function to execute.
+            params (dict): Parameters to pass to the function.
+            data (dict): Additional data to pass to the function.
+            key (str): A unique key to identify the task.
+            is_done_callback (callable): A callback function that will be executed after the task is done.
+        Returns: 
+            Void
+    """
     def submit(self, fn, params, data, key, is_done_callback):
         future = super().submit(fn, params, data)
         future.add_done_callback(is_done_callback)
