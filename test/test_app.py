fe1279b00219ffad764fc91bfa8770c03be1bec5
radufalcusan <radu.falcusan@anaqor.io>
2024-03-12 13:31:12 +0100

    remove unnecessary test_user_code file and add tests for delete execution

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,3 @@
-import json
 from fastapi.testclient import TestClient
 
 import sys
@@ -6,7 +5,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.app import app
 from src.model.execution_status import ExecutionStatus
-from src.model.job import Job
 
 client = TestClient(app)
 
@@ -23,7 +21,7 @@ def test_create_with_string_input_data():
         "data": {"test_data": "test_data_value2"},
         "params": {"test_param": "test_param_value2"}
     }
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -35,7 +33,7 @@ def test_create_with_object_input_data():
         "params": {"value2" : ["abc", "dce"]}
     }
     
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -45,7 +43,7 @@ def test_create_with_invalid_input_data():
     input_data = {
     }
     
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 422
     
@@ -67,7 +65,7 @@ def test_get_status_with_valid_execution():
         "params": {}
     }
 
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -77,7 +75,7 @@ def test_get_status_with_valid_execution():
     response = client.get(f'/{id}')
     # then
     assert response.status_code == 200
-    assert response.json()['status'] == ExecutionStatus.RUNNING or response.json()['status'] == ExecutionStatus.SUCCEEDED
+    assert response.json()['status'] == ExecutionStatus.SUCCEEDED or response.json()['status'] == ExecutionStatus.RUNNING
 
 def test_get_status_with_invalid_execution():
     # given
@@ -88,7 +86,7 @@ def test_get_status_with_invalid_execution():
         "params": {}
     }
 
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -100,7 +98,15 @@ def test_get_status_with_invalid_execution():
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.FAILED or response.json()['status'] == ExecutionStatus.RUNNING
 
+def test_get_result_throw_404_when_missing_execution():
+    # given
+    set_entry_point("test/test_user_code.valid.src.program:run")
 
+    # when
+    response = client.get(f'/123/result')
+    # then
+    assert response.status_code == 404
+    
 def test_get_result_of_valid_execution():
     # given
     set_entry_point("test/test_user_code.valid.src.program:run")
@@ -110,7 +116,7 @@ def test_get_result_of_valid_execution():
         "params": {}
     }
 
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -119,8 +125,8 @@ def test_get_result_of_valid_execution():
     # when
     response = client.get(f'/{id}/result')
     # then
-    print(response.json())
     assert response.status_code == 200
+    assert response.json()['result'] is not None
 
 def test_get_result_of_invalid_execution():
     # given
@@ -131,7 +137,7 @@ def test_get_result_of_invalid_execution():
         "params": {}
     }
 
-    response = client.post('/', data=json.dumps(input_data))
+    response = client.post('/', json=input_data)
 
     assert response.status_code == 200
     assert response.json()['status'] == ExecutionStatus.PENDING
@@ -141,3 +147,58 @@ def test_get_result_of_invalid_execution():
     response = client.get(f'/{id}/result')
     # then
     assert response.status_code == 500
+
+def test_delete_execution():
+    # given
+    set_entry_point("test/test_user_code.valid.src.program:run")
+
+    input_data = {
+        "data": {},
+        "params": {}
+    }
+
+    response = client.post('/', json=input_data)
+
+    assert response.status_code == 200
+    assert response.json()['status'] == ExecutionStatus.PENDING
+    id = response.json()['id']
+    assert id is not None
+    
+    response = client.get(f'/{id}')
+    assert response.status_code == 200
+    assert response.json()['status'] == ExecutionStatus.RUNNING or response.json()['status'] == ExecutionStatus.SUCCEEDED
+    
+    # when
+    response = client.delete(f'/{id}/cancel')
+    
+    # then
+    response = client.get(f'/{id}')
+    assert response.status_code == 404
+    
+def test_delete_execution_throw_404_when_missing_execution():
+    # given
+    set_entry_point("test/test_user_code.valid.src.program:run")
+
+    # when
+    response = client.delete('/123/cancel')
+    # then
+    assert response.status_code == 404
+    
+
+def test_delete_execution_with_wrong_id():
+    # given
+    set_entry_point("test/test_user_code.valid.src.program:run")
+
+    # when
+    response = client.delete('/123/cancel')
+    
+    # then
+    assert response.status_code == 404
+    
+def test_get_interim_result():
+    # given
+    set_entry_point("test/test_user_code.valid.src.program:run")
+
+    response = client.get(f'/{id}/interim-results')
+    
+    assert response.json() == []
