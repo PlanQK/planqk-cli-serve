a4b9b01f42ebc6b474fe07ec4a373755df72f0d7
radufalcusan <radu.falcusan@anaqor.io>
2024-03-20 16:30:59 +0100

    handle case in which dependencies have been updated after being previously installed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 if [ ! -f dependencies.txt ] || ! cmp -s dependencies.txt /user_code/requirements.txt; then
     pip3 install -r /user_code/requirements.txt
     cp /user_code/requirements.txt dependencies.txt
+elif cmp -s dependencies.txt /user_code/requirements.txt; then
+    exec "$@"
 else
+    pip3 uninstall -r /user_code/requirements.txt
+    pip3 install -r /user_code/requirements.txt
+    cp /user_code/requirements.txt dependencies.txt
     exec "$@"
 fi
