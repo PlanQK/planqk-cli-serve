f189f7552291f4105f3579c05942d4f84813530d
radufalcusan <radu.falcusan@anaqor.io>
2024-03-26 17:58:00 +0100

    Add error handling in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -e
+trap 'echo "Error: Command failed with exit code $?" >&2; exit 1' ERR
+
 if [ -f dependencies.txt ] && cmp -s dependencies.txt /user_code/requirements.txt; then
   exec "$@"
 elif [ -f dependencies.txt ] && ! cmp -s dependencies.txt /user_code/requirements.txt; then
-  echo "Rebuilding..."
+  cp /user_code/requirements.txt dependencies.txt
   pip3 uninstall -r /user_code/requirements.txt -y
   pip3 install -r /user_code/requirements.txt
-  cp /user_code/requirements.txt dependencies.txt
   exec "$@"
 else
-  pip3 install -r /user_code/requirements.txt
   cp /user_code/requirements.txt dependencies.txt
+  pip3 install -r /user_code/requirements.txt
 fi
