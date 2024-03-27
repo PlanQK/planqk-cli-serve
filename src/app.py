297475b9c512b13773c700cf3702512f2a541d39
radufalcusan <radu.falcusan@anaqor.io>
2024-03-27 17:49:06 +0100

    improve api-spec

--- src/app.py
+++ src/app.py
@@ -1,30 +1,37 @@
 from fastapi import BackgroundTasks, FastAPI
 from src.execution_manager import create_execution, delete_execution, get_execution_result, get_execution_status
 from src.model.execution_input import ExecutionInput
+import json
+
+app = FastAPI(
+title="Service API",
+    version="1.0",
+)
+
+with open('api-spec.json', 'r') as json_file:
+    api_spec = json.load(json_file)
 
 
-app = FastAPI(title=__name__)
-
-@app.get('/')
+@app.get('/', summary="Health checking endpoint", tags=["Status API"], responses=api_spec['paths']['/']['get']['responses'])
 def health_check():
     return ({"status": "Service is up and running"})
 
-@app.post('/')
+@app.post('/', summary="Asynchronous execution of the service", tags=["Service API"], responses=api_spec['paths']['/']['post']['responses'])
 async def create(input: ExecutionInput, background_tasks: BackgroundTasks):
     return create_execution(input, background_tasks)
 
-@app.get('/{id}')
+@app.get('/{id}', summary="Check execution status", tags=["Service API"], responses=api_spec['paths']['/{id}']['get']['responses'])
 def get_status(id):
     return get_execution_status(id)
 
-@app.get('/{id}/result')
+@app.get('/{id}/result', summary="Get the result of an execution", tags=["Service API"], responses=api_spec['paths']['/{id}/result']['get']['responses'])
 def get_result(id):
     return get_execution_result(id)
 
-@app.get('/{id}/interim-results')
+@app.get('/{id}/interim-results', summary="Get interim results of an execution", tags=["Service API"], responses=api_spec['paths']['/{id}/interim-results']['get']['responses'])
 def get_interim_results(id):
     return []
 
-@app.delete('/{id}/cancel')
+@app.put('/{id}/cancel', summary="Cancel an execution", tags=["Service API"], responses=api_spec['paths']['/{id}/cancel']['put']['responses'])
 def delete(id):
     delete_execution(id)
