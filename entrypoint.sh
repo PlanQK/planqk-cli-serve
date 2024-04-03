a8191cfa0ee12bcfce1f3e3fd2bd1308603f0618
radufalcusan <radu.falcusan@anaqor.io>
2024-04-03 12:57:13 +0200

    simplify dependency installation logic in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ trap 'echo "Error: Command failed with exit code $?" >&2; exit 1' ERR
 
 if [ -f dependencies.txt ] && cmp -s dependencies.txt /user_code/requirements.txt; then
   exec "$@"
-elif [ -f dependencies.txt ] && ! cmp -s dependencies.txt /user_code/requirements.txt; then
-  cp /user_code/requirements.txt dependencies.txt
-  pip3 uninstall -r /user_code/requirements.txt -y
-  pip3 install -r /user_code/requirements.txt
 else
   cp /user_code/requirements.txt dependencies.txt
   pip3 install -r /user_code/requirements.txt
