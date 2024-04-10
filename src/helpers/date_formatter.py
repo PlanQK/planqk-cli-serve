1dad80feaac06d7448347a3cdf1fc3f07b5ffad2
Michael Wurster <miwurster@googlemail.com>
2024-04-10 13:52:37 +0200

    chore: adjust error message

--- src/helpers/date_formatter.py
+++ src/helpers/date_formatter.py
@@ -2,11 +2,10 @@ from datetime import datetime
 
 from fastapi import HTTPException
 
+
 def format_timestamp(timestamp_float):
     try:
         timestamp_datetime = datetime.fromtimestamp(timestamp_float)
-        
         return timestamp_datetime.strftime("%Y-%m-%d %H:%M:%S")
-
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")    
+        raise HTTPException(status_code=500, detail=f"Error formatting timestamp: {str(e)}")
